627b30e456f8cca9b88309c3b2949cc7b00a8804
Alex Ferreira <aferreira91@gmail.com>
2014-12-31 10:21:47 -0300

    Optimization

--- src/main/java/br/uece/memcached/orchestrator/management/ResourcePool.java
+++ src/main/java/br/uece/memcached/orchestrator/management/ResourcePool.java
@@ -1,18 +1,14 @@
 package br.uece.memcached.orchestrator.management;
 
-import java.util.HashSet;
-import java.util.Set;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class ResourcePool<T> {
 
 	private BlockingQueue<T> idleResources;
-	private Set<T> busyResources;
 	
 	public ResourcePool(Integer size, ResourceBuilder<T> resourceBuilder) {
 		this.idleResources = new LinkedBlockingQueue<T>();
-		this.busyResources = new HashSet<T>();
 		
 		for (int i = 0; i < size; i++) {
 			idleResources.add(resourceBuilder.build());
@@ -21,10 +17,7 @@ public class ResourcePool<T> {
 	
 	public T getResource() {
 		try {
-			T resource = idleResources.take();
-			busyResources.add(resource);
-			
-			return resource;
+			return idleResources.take();
 		}
 		catch (InterruptedException e) {
 			throw new RuntimeException(e);
@@ -32,9 +25,7 @@ public class ResourcePool<T> {
 	}
 	
 	public void returnResource(T resource) {
-		if (busyResources.remove(resource)) {
-			idleResources.add(resource);
-		}
+		idleResources.add(resource);
 	}
 	
 	public interface ResourceBuilder<T> {
